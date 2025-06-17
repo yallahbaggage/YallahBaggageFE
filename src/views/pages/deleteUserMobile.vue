d84192399b0ccd2ffffaf882d45812d07ce5566b
RaoufSEZAR <raoufsato999@gmail.com>
2025-06-17 17:33:13 +0300

    add style

--- src/views/pages/deleteUserMobile.vue
+++ src/views/pages/deleteUserMobile.vue
@@ -1,35 +1,34 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
-import { useUserStore } from '@/stores/modules/userStore';
-import { errorMessage } from '@/utils/helpers';
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { useUserStore } from '@/stores/modules/userStore'
+import { errorMessage } from '@/utils/helpers'
 
-const name = ref('');
-const password = ref('');
-const passwordAgain = ref('');
-const email = ref(''); 
-const userStore = useUserStore();
+const name = ref('')
+const password = ref('')
+const passwordAgain = ref('')
+const email = ref('')
+const userStore = useUserStore()
 
 const deleteUser = async () => {
-  console.log(name.value, password.value, passwordAgain.value,email.value);
+  console.log(name.value, password.value, passwordAgain.value, email.value)
   if (password.value !== passwordAgain.value) {
-    errorMessage("Passwords do not match");
-    return;
+    errorMessage('Passwords do not match')
+    return
   }
   try {
-    await userStore.deleteUserByNameEmailAndPass(name.value, password.value, email.value);
+    await userStore.deleteUserByNameEmailAndPass(name.value, password.value, email.value)
   } catch (error) {
-    errorMessage("Failed to delete user");
-    console.log(error);
-    navigateTo.push('/');
-    }
+    errorMessage('Failed to delete user')
+    console.log(error)
+    navigateTo.push('/')
+  }
 }
 
-const navigateTo = useRouter();
+const navigateTo = useRouter()
 </script>
 <template>
-  <div>
-    <h1>Delete User</h1>
+  <div class="delete-user-mobile">
     <v-form @submit.prevent="deleteUser">
       <h1>Mobile User Account Delete</h1>
       <v-text-field v-model="name" label="Name" required />
@@ -37,8 +36,26 @@ const navigateTo = useRouter();
 
       <v-text-field v-model="password" label="Password" required />
       <v-text-field v-model="passwordAgain" label="Password Again" required />
-      <v-btn type="submit" color="error">Delete</v-btn>
-      <v-btn type="button" color="primary" @click="navigateTo.push('/')">Cancel</v-btn>
+      <div class="action-delete-btn">
+        <v-btn type="submit" color="error">Delete</v-btn>
+        <v-btn type="button" color="" @click="navigateTo.push('/')">Cancel</v-btn>
+      </div>
     </v-form>
   </div>
 </template>
+
+<style scoped>
+.delete-user-mobile {
+  max-width: 400px;
+  margin: auto;
+  padding: 20px;
+  gap: 10px;
+  display: flex;
+}
+
+.action-delete-btn {
+  display: flex;
+  gap: 10px;
+  margin-bottom: 20px;
+}
+</style>
