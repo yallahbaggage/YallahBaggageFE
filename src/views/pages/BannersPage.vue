3b82c975d5629e81e8df7919e88f3daa075b5ec8
RaoufSEZAR <raoufsato999@gmail.com>
2025-06-26 13:44:05 +0300

    fix icons

--- src/views/pages/BannersPage.vue
+++ src/views/pages/BannersPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app-management-page">
     <BaseHeader
-      pageImage="/src/assets/images/banner.svg"
+      :pageIcon="'mdi-image-area'"
       :title="t('banners')"
       :desc="t('manageYourAppsBanners')"
       :show-button="true"
