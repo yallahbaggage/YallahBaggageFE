a98b3a2ec5776dc120c2dfb6e81d0f9ca74e793c
RaoufSEZAR <raoufsato999@gmail.com>
2025-09-06 13:40:34 +0300

    fix assign driver style

--- src/components/base/AssignEmployeeCard.vue
+++ src/components/base/AssignEmployeeCard.vue
@@ -108,7 +108,7 @@ function statusColor(status: string): string {
   color: #fff;
   border: none;
   padding: 6px 16px;
-  border-radius: 12px;
+  border-radius: 8px;
   font-weight: 500;
   font-size: 14px;
   cursor: pointer;
